d27b9e1b8897c5eed21c5f978c6dc6ca27fa7e27
Vinicius Bloise <bloisv@usi.ch>
2020-11-03 20:00:44 +0100

    .ignore

--- paxos/connection.py
+++ paxos/connection.py
@@ -4,6 +4,8 @@ import struct
 
 def mcast_receiver(hostport):
   """create a multicast socket listening to the address"""
+  print('HostPost: ' + "\t".join([str(x) for x in hostport]))
+  #print('IPPROTO_UDP {}'.format(socket.IPPROTO_UDP))
   recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
   recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
   recv_sock.bind(hostport)
