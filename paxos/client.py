43c437f6cd44665da612ac1d0190030964d6e8be
Giorgio <giorgio_mcd@live.it>
2020-11-03 19:42:09 +0100

    all first 3 check OK also on Linux

--- paxos/client.py
+++ paxos/client.py
@@ -1,6 +1,7 @@
 import queue
 import socket
 import sys
+import time
 from connection import mcast_receiver, mcast_sender
 
 def client(config, id):
@@ -10,12 +11,21 @@ def client(config, id):
 
 
   s = mcast_sender()
+  i = 0
   for value in sys.stdin:
+    if (i%100 == 0):
+      if(id == 1):
+        s.sendto("startPaxos None None None None".encode(), config["proposers"])
+      time.sleep(0.5)
+    print("Client id: ", id ,", I'm asking to the proposers to start the battle.")
+    
+
+    i+=1
     value = value.strip()
     print ("client",id, ": sending ", value, " to proposers")
     value = "submit " + value + " " + str(id)+ " None None"
     s.sendto(value.encode(), config['proposers'])
+  time.sleep(5)
   if(id == 1):
-    print("Client id: ", id ,", I'm asking to the proposers to start the battle.")
     s.sendto("startPaxos None None None None".encode(), config["proposers"])
   print ("Client ",id," my job is done.")
