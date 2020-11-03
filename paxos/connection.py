4b8fac4654bd5baec0c1bebde6f0d5a224399c26
Vinicius Bloise <bloisv@usi.ch>
2020-11-03 18:38:02 +0100

    Correction for reuseport

--- paxos/connection.py
+++ paxos/connection.py
@@ -5,7 +5,7 @@ import struct
 def mcast_receiver(hostport):
   """create a multicast socket listening to the address"""
   recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-  recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
   recv_sock.bind(hostport)
 
   mcast_group = struct.pack("4sl", socket.inet_aton(hostport[0]), socket.INADDR_ANY)
